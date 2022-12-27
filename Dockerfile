72bd41de16d5d9e9ade15d8f9b45d1a262ae91b6
goflutterjava <goflutterjava@gmail.com>
2022-12-27 10:35:47 +0800

    add start log

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM ttbb/apisix:nake
 
+WORKDIR /opt/apisix
+
 COPY docker-build /opt/apisix/mate
 
 CMD ["/usr/bin/dumb-init", "bash", "-vx", "/opt/apisix/mate/scripts/start.sh"]
--- docker-build/scripts/start-apisix.sh
+++ docker-build/scripts/start-apisix.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 mkdir -p $APISIX_HOME/logs
+$APISIX_HOME/bin/apisix start -c $APISIX_HOME/conf/config.yaml >>$APISIX_HOME/logs/apisix.stdout.log 2>>$APISIX_HOME/logs/apisix.stderr.log &
