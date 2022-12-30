b57237de59510902642d1a18b5b20386f8f00e1b
Young Xu <xuthus5@gmail.com>
2022-12-30 16:47:21 +0800

    apisix env config and relatively work

--- docker-build/scripts/start-apisix.sh
+++ docker-build/scripts/start-apisix.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
 mkdir -p $APISIX_HOME/logs
-$APISIX_HOME/bin/apisix start -c $APISIX_HOME/conf/config.yaml >>$APISIX_HOME/logs/apisix.stdout.log 2>>$APISIX_HOME/logs/apisix.stderr.log &
+
+# fetch config from env
+config_path=$APISIX_HOME/conf/config.yaml
+
+echo "" > $config_path
+echo "apisix:" >> $config_path
+echo "  ssl:">> $config_path
+echo "    ssl_trusted_certificate: $SSL_TRUSTED_CERTIFICATE" >> $config_path
+echo "deployment:" >> $config_path
+echo "  etcd:" >> $config_path
+echo "    host:" >> $config_path
+etcd_hosts_array=(${ETCD_HOSTS//,/ })
+for host in ${etcd_hosts_array[@]}
+do
+  echo "      - $host" >> $config_path
+done
+echo "    prefix: \"apisix\"" >> $config_path
+echo "    tls:" >> $config_path
+echo "      cert: $ETCD_SERVER_CERT" >> $config_path
+echo "      key: $ETCD_SERVER_KEY" >> $config_path
+echo "      verify: true" >> $config_path
+cd $APISIX_HOME
+./bin/apisix start -c $APISIX_HOME/conf/config.yaml >>$APISIX_HOME/logs/apisix.stdout.log 2>>$APISIX_HOME/logs/apisix.stderr.log &
