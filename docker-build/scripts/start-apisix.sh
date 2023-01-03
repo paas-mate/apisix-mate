5921e2b5edb045befe292be8ce50200fa76ca377
goflutterjava <goflutterjava@gmail.com>
2023-01-03 10:56:23 +0800

    use my_config.yaml instead of config.yaml

--- docker-build/scripts/start-apisix.sh
+++ docker-build/scripts/start-apisix.sh
@@ -3,10 +3,9 @@
 mkdir -p $APISIX_HOME/logs
 
 # fetch config from env
-config_path=$APISIX_HOME/conf/config.yaml
+config_path=$APISIX_HOME/conf/my_config.yaml
 
-echo "" > $config_path
-echo "apisix:" >> $config_path
+echo "apisix:" > $config_path
 echo "  ssl:">> $config_path
 echo "    ssl_trusted_certificate: $SSL_TRUSTED_CERTIFICATE" >> $config_path
 echo "deployment:" >> $config_path
@@ -17,11 +16,11 @@ for host in ${etcd_hosts_array[@]}
 do
   echo "      - $host" >> $config_path
 done
-echo "    prefix: \"apisix\"" >> $config_path
+echo "    prefix: \"/apisix\"" >> $config_path
 echo "    tls:" >> $config_path
 echo "      cert: $ETCD_SERVER_CERT" >> $config_path
 echo "      key: $ETCD_SERVER_KEY" >> $config_path
-echo "      verify: true" >> $config_path
+echo "      verify: false" >> $config_path
 echo "  role: traditional" >> $config_path
 echo "  role_traditional:" >> $config_path
 echo "    config_provider: etcd" >> $config_path
@@ -32,4 +31,4 @@ CONF_ADMIN_KEY=${ADMIN_KEY:-edd1c9f034335f136f87ad84b625c8f1}
 echo "        key: $CONF_ADMIN_KEY" >> $config_path
 echo "        role: admin" >> $config_path
 cd $APISIX_HOME
-./bin/apisix start -c $APISIX_HOME/conf/config.yaml >>$APISIX_HOME/logs/apisix.stdout.log 2>>$APISIX_HOME/logs/apisix.stderr.log &
+./bin/apisix start -c $APISIX_HOME/conf/my_config.yaml >>$APISIX_HOME/logs/apisix.stdout.log 2>>$APISIX_HOME/logs/apisix.stderr.log &
